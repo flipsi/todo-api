90c973601e18002cd16c55584f04afa6f8d78ac3
Philipp Moers <soziflip@gmail.com>
2022-06-01 00:19:07 +0200

    Add exemplary data

---
+++ src/main/sql/INSERT_EXEMPLARY_DATA.sql
@@ -0,0 +1,8 @@
+INSERT INTO todos VALUES
+(234, 'Jagen', 'Im Wald und so :)'),
+(235, 'Essen', 'Echt lecker. Mjam.');
+
+INSERT INTO tasks VALUES
+(234, 'Erstmal packen.'),
+(234, 'Auf die Pirsch...'),
+(234, 'Schnapp es dir!');
