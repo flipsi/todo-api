72151e3f5e3f839db3f476528068ca9309250cad
Philipp Moers <soziflip@gmail.com>
2022-06-01 01:02:40 +0200

    Add remaining database methods

--- src/main/scala/org/sflip/todo-api/Database.scala
+++ src/main/scala/org/sflip/todo-api/Database.scala
@@ -20,7 +20,7 @@ object Database {
     val Password = "aTohdaeXeeng6eisha6aek"
   }
 
-  private val transactor = Transactor.fromDriverManager[IO](
+  private val xa = Transactor.fromDriverManager[IO](
     "org.postgresql.Driver",
     s"jdbc:postgresql:${ConnectionParameters.Database}",
     ConnectionParameters.Username,
@@ -30,27 +30,51 @@ object Database {
   // // CustomMappings allow to (de-)serialize custom types (such as Todo) directly (instead of tuples)
   // // TODO: make these work (currently fail with 'could not find implicit value for parameter ev')
   // private object CustomMappings {
-  //   implicit val todoGet: Get[Todo] = Get[(String, String)].map((name, description) => Todo(name, description, List.empty))
+  //   implicit val todoGet: Get[Todo] = Get[(String, String)].map((n, d) => Todo(n, d, List.empty))
   //   implicit val todoPut: Put[Todo] = Put[(String, String)].contramap(todo => (todo.name, todo.description))
   // }
   // import CustomMappings._
 
+  def getTodo(id: TodoId): Future[Option[Todo]] = getTodoIO(id).transact(xa).unsafeToFuture
+  def createTodo(todo: Todo): Future[TodoId] = createTodoIO(todo).transact(xa).unsafeToFuture
+  def deleteTodo(id: TodoId): Future[Unit] = deleteTodoIO(id).transact(xa).unsafeToFuture
+  def updateTodo(id: TodoId, todo: Todo): Future[Unit] = updateTodoIO(id, todo).transact(xa).unsafeToFuture
+  def addTask(id: TodoId, name: String): Future[Unit] = addTaskIO(id, name).transact(xa).unsafeToFuture
+  def deleteTask(id: TodoId, number: Int): Future[Unit] = deleteTaskIO(id, number).transact(xa).unsafeToFuture
+  def updateTask(id: TodoId, number: Int, name: String): Future[Unit] = updateTaskIO(id, number, name).transact(xa).unsafeToFuture
+
   // def getTodoIO(id: TodoId): ConnectionIO[Option[Todo]] =
-    // sql"""
+  // sql"""
   // SELECT todo.name, todo.description, array_agg(task.name)
   // FROM todos todo LEFT JOIN tasks task ON todo.id = task.todo_id
   // WHERE todo.id = $id
   // GROUP BY todo.id""".query[(String, String, List[String])].option map { _ map {
-    // case (name, description, tasks) => Todo(name, description, tasks.map(Task(_)))
+  // case (name, description, tasks) => Todo(name, description, tasks.map(Task(_)))
   // }}
 
   def getTodoIO(id: TodoId): ConnectionIO[Option[Todo]] =
     for {
-       todo <- sql" SELECT name, description FROM todos WHERE id = $id".query[(String, String)].option
-       tasks <- sql"SELECT name FROM tasks WHERE todo_id = $id".query[String].to[List]
-    } yield todo.map {case (name, description) => Todo(name, description, tasks.map(Task(_))) }
+      todo  <- sql" SELECT name, description FROM todos WHERE id = $id".query[(String, String)].option
+      tasks <- sql"SELECT name FROM tasks WHERE todo_id = $id".query[String].to[List]
+    } yield todo.map { case (name, description) => Todo(name, description, tasks.map(Task(_))) }
 
-  // def getTodo(id: TodoId): Future[Option[Todo]] = futurify { t => getTodoIO(id) }
-  def getTodo(id: TodoId): Future[Option[Todo]] = getTodoIO(id).transact(transactor).unsafeToFuture
+  def createTodoIO(todo: Todo): ConnectionIO[TodoId] =
+    sql"INSERT INTO todos (name, description) VALUES (${todo.name}, ${todo.description}".update
+      .withUniqueGeneratedKeys("id")
+
+  def deleteTodoIO(id: TodoId): ConnectionIO[Unit] =
+    sql"DELETE FROM todos WHERE id = $id".update.run.map(_ => ())
+
+  def updateTodoIO(id: TodoId, todo: Todo): ConnectionIO[Unit] =
+    sql"UPDATE todos SET name = ${todo.name}, description = ${todo.description} WHERE id = $id".update.run.map(_ => ())
+
+  def addTaskIO(id: TodoId, name: String): ConnectionIO[Unit] =
+    sql"INSERT INTO tasks (todo_id, name) VALUES ($id, $name)".update.run.map(_ => ())
+
+  // TODO: Solve problem of specifying tasks with number, as SQL tables are (unordered) sets!
+
+  def deleteTaskIO(id: TodoId, number: Int): ConnectionIO[Unit] = ???
+
+  def updateTaskIO(id: TodoId, number: Int, name: String): ConnectionIO[Unit] = ???
 
 }
